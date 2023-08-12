367586ebf30f087caeb23ec7050e443a8873591e
Fachrur Razi <fachrurrazi95@gmail.com>
2023-08-12 08:57:08 +0700

    remove unused store

--- src/utils/store/index.ts
+++ src/utils/store/index.ts
@@ -13,7 +13,6 @@ export const useAppStore = create<StoreState>()(persist((...params) => ({
 }), {
   name: "gogabook-app",
   partialize: (state) => ({
-    sidebarOpen: state.sidebarOpen,
     openMenu: state.openMenu,
     menuRoles: state.menuRoles,
     generalSettings: state.generalSettings,
